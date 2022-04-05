30540784a359f304306b34b7dde78b5670b952fb
Kexin <kllyu@student.unimelb.edu.au>
2022-04-05 18:09:46 +0800

    profile design finished

---
+++ src/components/profile/PersonalAvatar.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useContext } from "react";
+import avater from "../../images/fggf.jpg";
+import { Avatar, makeStyles } from "@material-ui/core";
+import Tooltip from "@material-ui/core/Tooltip";
+import conn from "../../util/conn";
+import AuthContext from "../../context/auth-context";
+
+const useStyles = makeStyles((theme) => ({
+	avatar: {
+		width: "150px",
+		height: "150px",
+	},
+	tooltip: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+}));
+
+function TopProfileAvatar() {
+	const classes = useStyles();
+	// const auth = useContext(AuthContext);
+
+	// const [avatarImage, setAvatarImage] = React.useState(avater);
+	// const imageHandler = (e) => {
+	// 	const reader = new FileReader();
+	// 	reader.onload = () => {
+	// 		if (reader.readyState === 2) {
+	// 			setAvatarImage(reader.result);
+	// 			// console.log(reader.result)
+	// 			var formdata = new FormData();
+	// 			formdata.append("image", e.target.files[0]);
+	// 			conn.post(`/user/uploadUserPhoto/${auth.userID}`, formdata)
+	// 				.then((res) => {
+	// 					console.log(res);
+	// 				})
+	// 				.catch((err) => {
+	// 					console.log(err);
+	// 				});
+	// 		}
+	// 	};
+	// 	reader.readAsDataURL(e.target.files[0]);
+	// };
+
+	// useEffect(() => {
+	// 	conn.get(`/user/getUser/${auth.userID}`)
+	// 		.then((user) => {
+	// 			if (user.data.image !== null && user.data.image !== undefined) {
+	// 				setAvatarImage(user.data.image);
+	// 			}
+	// 		})
+	// 		.catch((e) => {
+	// 			console.log(e);
+	// 		});
+	// }, [auth.userID]);
+
+	return (
+		<div className={classes.tooltip}>
+			<label htmlFor="avatar-upload">
+				<input
+					id="avatar-upload"
+					type="file"
+					accept="image/*"
+					// onChange={imageHandler}
+					hidden
+				/>
+				<Tooltip placement="top" title="Upload avatar">
+					<Avatar 
+						// src={avatarImage} 
+						scr={avater}
+						className={classes.avatar} />
+				</Tooltip>
+			</label>
+		</div>
+	);
+}
+
+export default TopProfileAvatar;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,20 +1,120 @@
-import LoggedInNav from "../components/common/LoggedInNav"
+import { makeStyles, Container, Grid } from "@material-ui/core";
 
+import LoggedInNav from "../components/common/LoggedInNav"
+import TopProfileAvatar from "../components/profile/PersonalAvatar";
+import './Profile.css'
+
+const useStyles = makeStyles((theme) => ({
+	container: {
+		display: "flex",
+		flexWrap: "wrap",
+		alignItems: "left",
+		padding: "20px",
+	},
+	textField: {
+		marginLeft: theme.spacing(1),
+		marginRight: theme.spacing(1),
+		width: 200,
+	},
+}));
 
 const Profile = () => {        
+
+	const classes = useStyles();
+
+	const agentVoice = [
+		// gonna change it later when get from the database
+		"Female-Russian",
+		"Male - American",
+		"Female - British"
+	];
+
+	const agentPersonality = [
+		"Friend",
+		"Aunt",
+		"Butler",
+		"Admirer"
+	]
    
     return (
 		<div>
+			<div className="background"></div>
 			<LoggedInNav/>
-			<div style={{
-					position: "absolute",
-					top: "30vh",
-					right: "0",
-					left: "0",
-					color: "black",
-					fontSize: "150%",
-					textAlign: "center"
-			}}>This is the Profile page, will add on this later</div>
+
+
+			<div className='dashboardMain'>
+
+
+				<h1>
+					<span>
+						{/* {" "}
+						{connection.firstname +
+							" " +
+							connection.lastname}{" "} */}
+						Hello, Lucy
+					</span>
+					{/* : "loading"} */}
+				</h1>
+
+				
+
+			<form className="form-profile" 
+				// onSubmit={handleSubmit}
+			>
+				<div className="avatar">
+					<TopProfileAvatar/>
+				</div>
+				
+				<div className="profile-container">
+					<div class="field">
+						<div class="field_label">Name</div>
+						<div class="input-wrapper">
+							<input 
+								type="text"
+								class="input" 
+								name="name"
+								value="Lucy"></input>
+						</div>
+					</div>
+
+					<div class="field">
+						<div class="field_label">Password</div>
+						<div class="input-wrapper">
+							<input 
+								type="password"
+								class="input" 
+								name="password"
+								value="123456"></input>
+						</div>
+					</div>
+
+					<div class="field">
+						<div class="field_label">Choose your agent voice</div>
+						<div class="input-wrapper">
+							<select class="selector" name="agentVoice">
+								<option value="female-russian">Female-Russian</option>
+								<option value="male-american">Male - American</option>
+								<option value="female-british">Female - British</option>
+							</select>
+						</div>
+					</div>
+
+					<div class="field">
+						<div class="field_label">Choose your agent personality</div>
+						<div class="input-wrapper">
+							<select class="selector" name="agentPersonality">
+								<option value="friend">Friend</option>
+								<option value="aunt">Aunt</option>
+								<option value="butler">Butler</option>
+								<option value="admirer">Admirer</option>
+							</select>
+						</div>
+					</div>
+
+	
+				</div>
+			</form>
+            </div>
 		</div>
     );
 
