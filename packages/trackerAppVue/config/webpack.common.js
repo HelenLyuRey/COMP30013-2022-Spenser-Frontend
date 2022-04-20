bedf6688de0b1ccc8f1092d1d01afefc11128195
Kexin <kllyu@student.unimelb.edu.au>
2022-04-20 19:16:53 +0800

    trackerApp works using webpack config as start script

---
+++ packages/trackerAppVue/config/webpack.common.js
@@ -0,0 +1,67 @@
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime'],
+                    }
+                }
+            }, {
+                test: /\.(sass|css|scss)$/,
+                use: [
+                  'style-loader',
+                  'css-loader',
+                  {
+                    loader: "postcss-loader",
+                    options: {
+                      plugins: () => [
+                        require("autoprefixer")()
+                      ],
+                    },
+                  },
+                  'sass-loader',
+                ]
+              },{
+                test: /\.svg$/,
+                use: [
+                  {
+                    loader: require.resolve('@svgr/webpack'),
+                    options: {
+                      prettier: false,
+                      svgo: false,
+                      svgoConfig: {
+                        plugins: [{ removeViewBox: false }],
+                      },
+                      titleProp: true,
+                      ref: true,
+                    },
+                  },
+                  {
+                    loader: require.resolve('file-loader'),
+                    options: {
+                      name: 'static/media/[name].[hash].[ext]',
+                    },
+                  },
+                ],
+                issuer: {
+                  and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+                },
+              },
+        ]
+    },
+    plugins: [new VueLoaderPlugin()]
+}
---
+++ packages/trackerAppVue/src/bootstrap.js
@@ -0,0 +1,45 @@
+import { createApp } from 'vue'
+import App from './App'
+
+import config from './config'
+import translations from './translations/translations.json'
+import { registerServiceWorker } from './utils'
+
+const mount = (el) => {
+    const app = createApp(App)
+
+    registerServiceWorker()
+
+    app.config.globalProperties.config = config // <- set config to global scope
+    app.config.globalProperties.translations = translations // <- set translations to global scope
+
+    /* (global) This code is going to tell us, if history mode can be activated on the client, so the application can be consumed without sessionStorage */
+    app.config.globalProperties.history = () => {
+    try {
+        sessionStorage.getItem('check')
+        return true
+    } catch {
+        return false
+    }
+    }
+
+    /* (global) Currently selected language or fallback language (en). Needs to be a function, since it's reactive. No need for vuex there */
+    app.config.globalProperties.lang = () => {
+    if (app.config.globalProperties.history()) return sessionStorage.getItem('lang') || config.fallback_lang
+        return config.fallback_lang
+    }
+
+    /* (global) Debug mode */
+    app.config.globalProperties.debug = () => process.env.NODE_ENV === 'development'
+    app.mount(el)
+}
+
+
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#tracker-vue-app')
+    if (devRoot) {
+        mount(devRoot)
+    }
+}
+
+export { mount }
